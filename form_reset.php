3af1c41092c67de67de0252162fc038451df2be1
Ivan Ferré <ivanferre@gmail.com>
2014-03-26 14:53:36 +0100

    Cleanup and correct form id

--- form_reset.php
+++ form_reset.php
@@ -2,43 +2,44 @@
 <html>
 <head>
 	<style>
-		font-family: {courier, sans-serif;}
-		font-size:	{14px;}
-		.error {color: #FF0000;}
+	body {
+		font-family:	sans-serif;
+		font-size:		14px;
+	}
+	/*.error {color: #FF0000;}*/
 	</style>
     <script>
-		function clean_hardware_form ()
+		function clean_hw_form ()
 		{
-			document.forms["hardware_form"].reset();
-			alert("clean_hardware_form()");
-			document.hardware_form.getElementById("name").value = "";
-			document.hardware_form.getElementById("car").selected = "1";
+			document.forms["hw_form"].reset();
+			alert("clean_hw_form()");
+			document.hw_form.getElementById("name").value = "";
+			document.hw_form.getElementById("car").selected = "1";
 			alert("form reseteado.");
 		}
 
 		function checkForm (myForm)
-		{
-			var myForm	= document.getElementById(myFormId);
-			var str		= "";
-
-			alert ("Checking...");
-			str.concat (str, "Name = ", document.hardware_form.getElementById("name").value, "\n");
-			str.concat (str, "Gender = ", document.hardware_form.getElementById("gender").value, "\n");
-			str.concat (str, "Car = ", document.hardware_form.getElementById("car").value, "\n");
-			str.concat (str, "Feature = ", document.hardware_form.getElementById("feature").value, "\n");
-			alert (str);
+		{			
+			document.hw_form.getElementById("name").value		= "";
+			document.hw_form.getElementById("gender").value	= "";
+			document.hw_form.getElementById("car").value		= "";
+			document.hw_form.getElementById("feature").value	= "";
 		}
 
-		function clearElements(el) {
+		function clearElements(el)
+		{
 		    // variable declaration
 		    var x, y, z, type = null, object = [];
 		    // collect form elements
 		    object[0] = document.getElementById(el).getElementsByTagName('input');
 		    object[1] = document.getElementById(el).getElementsByTagName('textarea');
 		    object[2] = document.getElementById(el).getElementsByTagName('select');
+		
 		    // loop through found form elements
-		    for (x = 0; x < object.length; x++) {
-		        for (y = 0; y < object[x].length; y++) {
+		    for (x = 0; x < object.length; x++)
+			{
+		        for (y = 0; y < object[x].length; y++)
+				{
 		            // define element type
 		            type = object[x][y].type;
 		            switch (type) {
@@ -65,7 +66,7 @@
 		}
 
 
-	   function resetForm(myFormId)
+	   function emptyForm(myFormId)
 	   {
 		   var myForm = document.getElementById(myFormId);
 
@@ -89,84 +90,57 @@
 
 $debugstr = "";
 
-function resetParameters()
+function emptyForm()
 {
-	$GLOBALS['debugstr'] .= "resetParameters()<br />";
+	$GLOBALS['debugstr'] .= "emptyForm()<br />";
 	unset ($_GET) ;
 	$GLOBALS['name'] = $GLOBALS['gender'] = $GLOBALS['car'] = $GLOBALS['feature'] = "";
 	$GLOBALS['debugstr'] .= "Variables unset.<br />";
 
 	echo "<script>
 		alert('cleaning...');
-	   	var myForm = document.getElementById('hardware_form');
+	   	var myForm = document.getElementById('hw_form');
 
-       	alert ('resetForm() runs');
-       	for (var i = 0; i < hardware_form.elements.length; i++)
+       	alert ('emptyForm() runs');
+       	for (var i = 0; i < hw_form.elements.length; i++)
        	{
-           if ('submit' != hardware_form.elements[i].type && 'reset' != hardware_form.elements[i].type)
+			switch (hw_form.elements[i].type)
+			{
+			case 'submit':
+			case 'reset':
+			case 'button':
+				/* do nothing */
+				break;
+			default:
+                hw_form.elements[i].checked = false;
+                hw_form.elements[i].value = '';
+                hw_form.elements[i].selectedIndex = 0;
+			}
+			/***
+           if ('submit' != hw_form.elements[i].type && 'reset' != hw_form.elements[i].type)
            {
-               hardware_form.elements[i].checked = false;
-               hardware_form.elements[i].value = '';
-               hardware_form.elements[i].selectedIndex = 0;
+               hw_form.elements[i].checked = false;
+               hw_form.elements[i].value = '';
+               hw_form.elements[i].selectedIndex = 0;
            }
+			***/
        	}
 		</script>";
-
 }
 
 
 
 // define variables and set to empty values
 $name = $gender = $car = $feature = "";
-$nameErr = $genderErr = $carErr = $featureErr = "";
 $selected = "selected=\"selected\"";
 
 
 if ($_SERVER["REQUEST_METHOD"] == "GET")
 {
-	if (empty($_GET["name"]))
-	{
-		$nameErr = "Name is required";
-	}
-	else
-	{
-		$name = test_input($_GET["name"]);
-	  	// check if name only contains letters and whitespace
-	  	if (!preg_match("/^[a-zA-Z ]*$/",$name))
-		{
-			$nameErr = "Only letters and white space allowed"; 
-		}
-	}
-
-	if (empty ($_GET["gender"]))
-	{
-		$genderErr = "Gender is required";
-	}
-	else
-	{
-		$gender = test_input ($_GET["gender"]);
-	}
-	
-
-	if (empty($_GET["car"]))
-	{
-		$car = "";
-	}
-	else
-	{
-		$car = test_input($_GET["car"]);
-	}
-
-	if (empty($_GET["feature"]))
-	{
-		$featureErr = "Feature is empty.";
-		$feature = "off";
-	}
-	else
-	{
-		$feature = test_input($_GET["feature"]);
-		$featureErr = $feature;
-	}
+	if (isset($_GET["name"]))		$name		= test_input($_GET["name"]);
+	if (isset($_GET["gender"]))		$gender		= test_input ($_GET["gender"]);
+	if (isset($_GET["car"]))		$car		= test_input($_GET["car"]);
+	if (isset($_GET["feature"]))	$feature	= test_input($_GET["feature"]);
 }
 
 
@@ -180,50 +154,59 @@ function test_input($data)
 ?>
 
 <h2>PHP Form Validation Example</h2>
-<p><span class="error">* required field.</span></p>
-<form id="hw_form" name="hardware_form" method="get" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+<form id="hw_form_id" name="hw_form_name" method="get" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 	
 	
-	Name: <input type="text" name="name" value="<?php echo $name;?>">
-	<span class="error">* <?php echo $nameErr;?></span>
-	<br><br>
-
-	Gender:
+<div>
+	<label for="name">Name:</label>
+	<input type="text" name="name" value="<?php echo $name;?>">
+</div>
+	<br>
+	<br>
+<div>
+	<label for="gender">Gender:</label>
 	<input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?>  value="female">Female
 	<input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?>  value="male">Male
-	<span class="error">* <?php echo $genderErr;?></span>
-	<br><br>
-
+</div>
+	<br>
+	<br>
+<div>
+	<label for="car">Gender:</label>
 	<select name="car" size="1">
 		<option value="volvo" <?php $str = ("volvo" == $car) ? $selected : "  " ; echo $str; ?>	 >Volvo</option>
 		<option value="saab" <?php $str = ("saab" == $car) ? $selected : "  " ; echo $str; ?>	 >Saab</option>
 		<option value="mercedes" <?php $str = ("mercedes" == $car) ? $selected : "  " ; echo $str; ?>	 >Mercedes</option>
 		<option value="audi" <?php $str = ("audi" == $car) ? $selected : "  " ; echo $str; ?>	 >Audi</option>
 	</select>
-	<br><br>
+</div>
+	<br>
+	<br>
 
+<div>
 	<label for="feature">Feature:</label>
 	<input type="checkbox" name="feature"
 		<?php if (isset($green) and $green == "on") echo "checked=\"checked\"" ?>
 >
-	<br><br>
-
-	<input type="submit" name="submit" value="Submit"> 
-	<input type="button" name="reset" value="Reset" onclick="resetForm('hardware_form')">
+	<br>
+	<br>
+</div>
+	<input type="submit" name="submit" value="Submit">
+	<input type="reset" />
+	<input type="button" name="empty" value="Empty" onclick="emptyForm('hw_form')">
 	<input type="button" name="check" value="Check" onclick="checkForm()">
-	<input type="button" name="clear" value="Clear" onclick="clearElements()">
+	<input type="button" name="clear" value="Clear" onclick="clearElements('hw_form')">
 </form>
 
 <?php
 echo "<h2>Your Input:</h2>";
 
-echo $name;
+echo "Name: " . $name;
 echo "<br>";
-echo $gender;
+echo "Gender: " . $gender;
 echo "<br>";
-echo $car;
+echo "Car: " . $car;
 echo "<br>";
-echo $feature;
+echo "Feature: " . $feature;
 echo "<br>";
 echo $debugstr;
 echo "<br>";
